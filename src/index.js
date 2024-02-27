8ce0a9b7ce1e96a8d7c4f3439643d82130c8359d
mafusailovich <vfesenko@yandex.ru>
2024-02-27 06:33:14 +0300

    2 screen + swiper

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import 'normalize.css';
 import './styles/main.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
